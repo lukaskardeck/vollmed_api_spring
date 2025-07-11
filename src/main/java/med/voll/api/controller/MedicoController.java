34518a3345bfa1a5be6c4a6aa7c7e7d55b5f6c89
lukaskardeck <lukas.kardeck@gmail.com>
2025-07-11 19:59:33 -0300

    feat: listagem de medicos com paginação e ordenação

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,14 +1,18 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medico")
@@ -19,7 +23,12 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void cadastro(@RequestBody DadosCadastroMedico dadosCadastroMedico) {
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dadosCadastroMedico) {
         medicoRepository.save(new Medico(dadosCadastroMedico));
+    }
+
+    @GetMapping
+    public Page<DadosListagemMedico> listar(@PageableDefault(sort = {"nome"}) Pageable paginacao) {
+        return medicoRepository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 }
