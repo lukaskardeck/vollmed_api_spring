9b0fec55fde439e6c7377999d159a77f77a0d0d3
lukaskardeck <lukas.kardeck@gmail.com>
2025-07-12 21:35:09 -0300

    feat: gerando Filters para controle de acesso

---
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -0,0 +1,38 @@
+package med.voll.api.infra.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+
+        var token = recuperarTokenJWT(request);
+        var subject = tokenService.getSubject(token);
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarTokenJWT(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null) {
+            throw new RuntimeException("Token JWT não enviado no cabeçalho 'Authorization'");
+        }
+
+        return authHeader.replace("Bearer", "");
+    }
+}
